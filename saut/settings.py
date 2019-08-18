c282f4944a3276b3559e9d013becd954727cbc40
arpit <arpit.oberoi@cloudsek.com>
2019-08-18 07:12:37 +0530

    gunicorn stuff

--- saut/settings.py
+++ saut/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uoyhejj+hwfg0zssg+6t$3^a(v2^vy5!w)--8sp3jx74l859he'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '[::1]', 'localhost']
+ALLOWED_HOSTS = ['107.155.135.16']
 
 # CUSTOM
 DATASET_PATH = os.path.join(BASE_DIR, 'word_search.tsv')
