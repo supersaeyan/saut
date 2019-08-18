c5d024cf74da22a3077f0521a1bdc9930e8b8ea6
arpit <arpit.oberoi@cloudsek.com>
2019-08-18 07:10:38 +0530

    gunicorn stuff

--- saut/settings.py
+++ saut/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uoyhejj+hwfg0zssg+6t$3^a(v2^vy5!w)--8sp3jx74l859he'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', '[::1]', 'localhost']
 
